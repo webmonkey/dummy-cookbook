d291c2473fbe808941235da7092c1a95f9cfd8df
James Cohen <james.cohen@gmail.com>
2014-04-28 17:16:34 +0100

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'dummy'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures dummy'
+long_description 'Installs/Configures dummy'
+version          '0.1.0'
+
+depends 'haproxy'
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: dummy
+# Recipe:: default
+#
+# Copyright (C) 2014 
+#
+# 
+#
+
+include_recipe "haproxy::default";
